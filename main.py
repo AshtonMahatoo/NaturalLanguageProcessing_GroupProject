401ee2240ddcb6ca952888afe1bfe48381fe5f16
r2d2well <93230257+r2d2well@users.noreply.github.com>
2024-10-09 18:35:12 -0400

    Updated the main.py
    Added 2 alternate vectorization functions that can be manually switched between to compare results (TfidfVectorizer or CountVectorizer) as well as records and displays the time it took for the model to run. Also updated how the accuracy is displayed so now it goes in-depth over the number of true positives, false positives, true negatives, and false negatives as well as the overall accuracy.

--- main.py
+++ main.py
@@ -1,20 +1,56 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer as vectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.feature_extraction.text import CountVectorizer
+import time
+
+def basic(testSet, trainSet):
+    vectorizer = TfidfVectorizer()
+
+    X_train = vectorizer.fit_transform(trainSet['text'])
+    X_test = vectorizer.transform(testSet['text'])
+
+    model = LogisticRegression()
+
+    model.fit(X_train, trainSet['label'])
+
+    y_pred = model.predict(X_test)
+
+    return confusion_matrix(testSet['label'], y_pred)
+
+def countVectorizer(testSet, trainSet):
+    vectorizer = CountVectorizer()
+    X_train = vectorizer.fit_transform(trainSet['text'])
+    X_test = vectorizer.transform(testSet['text'])
+
+    model = LogisticRegression()
+
+    model.fit(X_train, trainSet['label'])
+
+    y_pred = model.predict(X_test)
+
+    return confusion_matrix(testSet['label'], y_pred)
 
 testSet = pd.read_csv('test_amazon.csv')
 trainSet = pd.read_csv('train_amazon.csv')
+start_time = time.time()
 
-vectorizer = vectorizer()
-X_train = vectorizer.fit_transform(trainSet['text'])
-X_test = vectorizer.transform(testSet['text'])
+CM = basic(testSet, trainSet)
 
-model = LogisticRegression()
+end_time = time.time()
+elapsed_time = end_time - start_time
 
-model.fit(X_train, trainSet['label'])
+TP = CM[1][1]
+TN = CM[0][0]
+FP = CM[0][1]
+FN = CM[1][0]
 
-y_pred = model.predict(X_test)
+accuracy = ((TP + TN)/(TP + TN + FP + FN))
+print("True Positives: ", TP)
+print("True Negative: ", TN)
+print("False Positives: ", FP)
+print("False Negatives: ", FN)
+print("OVerall Accuracy: ", accuracy)
 
-accuracy = accuracy_score(testSet['label'], y_pred)
-print("Accuracy:", accuracy)
+print(f"Elapsed time: {elapsed_time} seconds")
